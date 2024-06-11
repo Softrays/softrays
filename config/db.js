7238728f8ad45bef45cb42db25c841978956f57a
Segcal <seguncaleb3@gmail.com>
2024-06-11 11:12:12 +0100

    All prices for all courses have been updated and on the form section few resposive changes have been made ....

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
 // SETTING UP THE DATABASE
-// "mongodb://localhost:27017/softraysDB"
-// process.env.MONGODB_CONNECTION_STRING
+ "mongodb://localhost:27017/softraysDB"
+ process.env.MONGODB_CONNECTION_STRING
 
-// "mongodb+srv://admin:admin@softrays.ojrey.mongodb.net/softraysDB"
+ "mongodb+srv://admin:admin@softrays.ojrey.mongodb.net/softraysDB"
 
 const db = async () => {
   await mongoose.connect(process.env.MONGODB_CONNECTION_STRING);
--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -30,16 +30,20 @@ function tooglePassword() {
   const password = document.querySelector("#password");
 
   togglePassword.addEventListener("click", (e) => {
-    const type = password.getAttribute("type") === "password" ? "text" : "password";
+    const type =
+      password.getAttribute("type") === "password" ? "text" : "password";
     password.setAttribute("type", type);
     togglePassword.classList.toggle("ri-eye-off-fill");
   });
 
-  const anotherTogglePassword = document.querySelector("#anotherTogglePassword");
+  const anotherTogglePassword = document.querySelector(
+    "#anotherTogglePassword"
+  );
   const anotherPassword = document.querySelector("#anotherPassword");
 
   anotherTogglePassword.addEventListener("click", (e) => {
-    const type = anotherPassword.getAttribute("type") === "password" ? "text" : "password";
+    const type =
+      anotherPassword.getAttribute("type") === "password" ? "text" : "password";
     anotherPassword.setAttribute("type", type);
     anotherTogglePassword.classList.toggle("ri-eye-off-fill");
   });
@@ -74,108 +78,108 @@ function changePrice() {
       document.getElementById("price").value = 0;
       break;
     case "Advance Certification in Microsoft Excel":
-      document.getElementById("price").value = 20000;
+      document.getElementById("price").value = 50000;
       document.getElementById("duration").value = "2 Weeks";
       break;
     case "Advance Certification in Solid Works":
-      document.getElementById("price").value = 40000;
+      document.getElementById("price").value = 60000;
       document.getElementById("duration").value = "6 Weeks";
       break;
     case "Advance Certification in AutoCAD (2D&3D)":
-      document.getElementById("price").value = 40000;
+      document.getElementById("price").value = 60000;
       document.getElementById("duration").value = "8 Weeks";
       break;
     case "Advance Certification in Revit (2D&3D)":
-      document.getElementById("price").value = 40000;
+      document.getElementById("price").value = 60000;
       document.getElementById("duration").value = "8 Weeks";
       break;
     case "Advance Certification in Python (Data Science)":
-      document.getElementById("price").value = 100000;
+      document.getElementById("price").value = 150000;
       document.getElementById("duration").value = "8 Weeks";
       break;
     case "Advance Certification in Java Programming":
-      document.getElementById("price").value = 40000;
+      document.getElementById("price").value = 150000;
       document.getElementById("duration").value = "8 Weeks";
       break;
     case "Advance Certification in Cyber Security":
-      document.getElementById("price").value = 100000;
+      document.getElementById("price").value = 150000;
       document.getElementById("duration").value = "12 Weeks";
       break;
     case "Advance Certification in Digital Marketing":
-      document.getElementById("price").value = 100000;
+      document.getElementById("price").value = 200000;
       document.getElementById("duration").value = "12 Weeks";
       break;
     case "Certification in Information Technology and Computing":
-      document.getElementById("price").value = 30000;
+      document.getElementById("price").value = 80000;
       document.getElementById("duration").value = "6 Weeks";
       break;
-    case "Certification in Adobe Photoshop":
-      document.getElementById("price").value = 30000;
+    case "Professional Certification in Adobe Photoshop":
+      document.getElementById("price").value = 60000;
       document.getElementById("duration").value = "8 Weeks";
       break;
     case "Computer Systems Engineering A+ and Networking N+ (CSE)":
-      document.getElementById("price").value = 40000;
+      document.getElementById("price").value = 80000;
       document.getElementById("duration").value = "8 Weeks";
       break;
     case "Certification in CorelDRAW and Digital Printing":
-      document.getElementById("price").value = 30000;
+      document.getElementById("price").value = 60000;
       document.getElementById("duration").value = "8 Weeks";
       break;
     case "Certificate in Python Programming":
-      document.getElementById("price").value = 40000;
+      document.getElementById("price").value = 60000;
       document.getElementById("duration").value = "6 Weeks";
       break;
     case "Certificate in Digital Marketing":
-      document.getElementById("price").value = 50000;
+      document.getElementById("price").value = 100000;
       document.getElementById("duration").value = "6 Weeks";
       break;
     case "Certificate in Motion Graphics":
-      document.getElementById("price").value = 50000;
+      document.getElementById("price").value = 100000;
       document.getElementById("duration").value = "8 Weeks";
       break;
     case "CCNA (CISCO CERTIFICATION)":
-      document.getElementById("price").value = 100000;
+      document.getElementById("price").value = 150000;
       document.getElementById("duration").value = "8 Weeks";
       break;
     case "CCNP (CISCO CERTIFICATION)":
-      document.getElementById("price").value = 100000;
+      document.getElementById("price").value = 150000;
       document.getElementById("duration").value = "8 Weeks";
       break;
-    case "Data Analysis Using SPSS":
-      document.getElementById("price").value = 35000;
+    case "Data Analytics":
+      document.getElementById("price").value = 150000;
       document.getElementById("duration").value = "6 Weeks";
       break;
     case "Internship":
-      document.getElementById("price").value = 20000;
+      document.getElementById("price").value = 50000;
       document.getElementById("duration").value = "8 Weeks";
       break;
     case "Mini Importation Training":
-      document.getElementById("price").value = 30000;
+      document.getElementById("price").value = 50000;
       document.getElementById("duration").value = "4 Weeks";
       break;
-    case "Mobile App Development":
-      document.getElementById("price").value = 50000;
+    case "Mobile App Development (Hybrid)":
+      document.getElementById("price").value = 150000;
       document.getElementById("duration").value = "12 Weeks";
       break;
     case "Sage Accounting":
-      document.getElementById("price").value = 50000;
+      document.getElementById("price").value = 80000;
       document.getElementById("duration").value = "6 Weeks";
       break;
     case "User Interface/User Experience (UI/UX) Desgin":
-      document.getElementById("price").value = 40000;
+      document.getElementById("price").value = 100000;
       document.getElementById("duration").value = "8 Weeks";
       break;
     case "Web Development (Front End)":
-      document.getElementById("price").value = 30000;
-      document.getElementById("duration").value = "6 Weeks";
+      document.getElementById("price").value = 150000;
+      document.getElementById("duration").value = "8 Weeks";
       break;
     case "Web Development (Full Stack)":
-      document.getElementById("price").value = 60000;
-      document.getElementById("duration").value = "12 Weeks";
+      document.getElementById("price").value = 300000;
+      document.getElementById("duration").value = "10 Weeks";
       break;
-    case "siwes":
+    case "siwes/IT":
       document.getElementById("price").value = 10000;
-      document.getElementById("duration").value = "12 Weeks";
+      document.getElementById("duration").value = "6 Months";
       break;
     default:
       console.log(course);
